960cc3845721fa7516ae6ce5d43e4f2dcd3dc1b6
Bruno <47203420+brunoa19@users.noreply.github.com>
2021-01-26 14:49:18 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,23 +29,3 @@ python2.7-dev
 
 pip install -q virtualenv==16.7.0
 pip install -q pyOpenSSL==19.1.0
-
-export JQ_VERSION=1.5*
-echo "================= Adding JQ $JQ_VERSION ========================="
-apt-get install -y -q jq="$JQ_VERSION"
-
-echo "================= Installing CLIs packages ======================"
-
-export GIT_VERSION=1:2.*
-echo "================= Installing Git $GIT_VERSION ===================="
-add-apt-repository ppa:git-core/ppa -y
-apt-get update -qq
-apt-get install -y -q git="$GIT_VERSION"
-
-
-JFROG_VERSION=1.33.2
-echo "================= Adding jfrog-cli $JFROG_VERSION  ================"
-wget -nv https://api.bintray.com/content/jfrog/jfrog-cli-go/"$JFROG_VERSION"/jfrog-cli-linux-amd64/jfrog?bt_package=jfrog-cli-linux-amd64 -O jfrog
-sudo chmod +x jfrog
-mv jfrog /usr/bin/jfrog
-
